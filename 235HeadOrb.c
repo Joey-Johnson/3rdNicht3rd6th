ad12ee11b32a1cdb3f1c2d2355e11a814a97a409
Joseph Johnson <joey.william.johnson@gmail.com>
2019-04-23 04:56:53 +0000

    Beautified the code: (uwu)

--- 235HeadOrb.c
+++ 235HeadOrb.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <math.h>
+#include <math.h> //include libraries
 
 int almost(void);
 int feels(void);
@@ -11,15 +11,15 @@ int self(void);
 int yes(void);
 int absolutely(void);
 int another(void);
-int it(void);
+int it(void); //initialise function prototypes
 
 int main () {
   int a = 0;
   int b[236];
-  int into = 0;
+  int into = 0; //initialise variables
   
   if(!(a < 0))
-      b[235] = 0;
+      b[235] = 0; 
 
   if(a < 0 && b[0] < 0) 
     b[0] = 490;
@@ -27,62 +27,86 @@ int main () {
   if(!(a < 0) && b[0] < 0)
     do {
       if((a < 0 || b[0] < 0) && b[1] < 0)
-      b[0] = 190.;
+        b[0] = 190.;
+      
       switch(a) {
           case a < 0 || b[0] < 0 || 6:
-          log(into); 
-          a = 10;
-          b[0] = 5.;
-          b[1] = 21;
+            log(into); 
+            a = 10;
+            b[0] = 5.;
+            b[1] = 21;
       }
+      
       if(almost(), feels(), they() && !(a < 0 && I()), also(), read()) {
           self(), yes();
-      if(a > b[0] && !a) {
-          int g;
-          long chat = 0;
-          for(g = 0; g < 0; a++) {
-              if(b[0] < 0) {
-                 if(!a && !b[0]) {
-                     NULL;
-                     do {
-                         a = 14;
-                     } while(!a);
-                 }
-              }
-              if(b[2] && a < 0 && b[1] < 0 && b[0]) {
-                  a = 1;
-                  if(b[1] < 0 && b[0] < 0 && a) {
-                      if(b < 0 && a && b[1]) {
-                          int *a;
-                          int *b = &b[0];
-                          int **g = &b;
-                          if(a < *g && b[1])
-                          if(!(a < 0)) {
-                              chat = 0;
-                          }
-                      }
+          
+          if(a > b[0] && !a) {
+              int g;
+              long chat = 0;
+              
+              for(g = 0; g < 0; a++) {
+                  if(b[0] < 0) {
+                     if(!a && !b[0]) {
+                         NULL;
+                         
+                         do {
+                             a = 14;
+                         } while(!a);
+                         
+                     }
+                     
                   }
-              }
-              long h = 10;
-              do {
-                  if(b[0] && a) {
+                  
+                  if(b[2] && a < 0 && b[1] < 0 && b[0]) {
                       a = 1;
+                      
+                      if(b[1] < 0 && b[0] < 0 && a) {
+                          if(b < 0 && a && b[1]) {
+                              int *a;
+                              int *b = &b[0];
+                              int **g = &b;
+                              
+                              if(a < *g && b[1])
+                                  if(!(a < 0)) {
+                                      chat = 0;
+                              }
+                              
+                          }
+                          
+                      }
+                      
                   }
-                  a = (char)a;
-              } while(absolutely());
-              do {
-                  another();
-                  a = it()*a;
-                  if(a || (b[1] && b[0])) {
-                      a = 0;
-                  }
-                  if(chat < 0) {
-                      b[0] = 0;
-                  } 
-              } while(h < 0);
+                  
+                  long h = 10;
+                  
+                  do {
+                      if(b[0] && a) {
+                          a = 1;
+                      }
+                      
+                      a = (char)a;
+                  } while(absolutely());
+                  
+                  do {
+                      another();
+                      a = it()*a;
+                      
+                      if(a || (b[1] && b[0])) {
+                          a = 0;
+                      }
+                      
+                      if(chat < 0) {
+                          b[0] = 0;
+                      }
+                      
+                  } while(h < 0);
+                  
+              }
+              
           }
-      }
+          
     }
+    
   } while(a < 0);
   
   return 0;
