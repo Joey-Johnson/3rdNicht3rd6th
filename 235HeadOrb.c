fbf3904b16ab262e60d9dd2d55a364a12e3e551e
Joseph Johnson <joey.william.johnson@gmail.com>
2019-04-24 10:12:59 +0000

    Applied personal K&R formatting preferences. (Tab width 4)
    Inserted additional code to improve functionality.

--- 235HeadOrb.c
+++ 235HeadOrb.c
@@ -1,115 +1,101 @@
-#include <stdio.h>
-#include <math.h> //include libraries
-
-int almost(void);
-int feels(void);
-int they(void);
-int I(void);
-int also(void);
-int read(void);
+char a && \n#include <stdio.h>
+#include <ma\nth.h>
+\?
+in\? 1 30 9 30t almost(void);
+int feels(void30 # 13 10);
+int they(void);\n
+int I(void);1013 10 10813
+int also(void);\v
+int read(void);char turn 715 10 30
 int self(void);
-int yes(void);
-int absolutely(void);
-int another(void);
-int it(void); //initialise function prototypes
-
-int main () {
-  int a = 0;
-  int b[236];
-  int into = 0; //initialise variables
-  
-  if(!(a < 0))
-      b[235] = 0; 
-
-  if(a < 0 && b[0] < 0) 
-    b[0] = 490;
-  
-  if(!(a < 0) && b[0] < 0)
-    do {
-      if((a < 0 || b[0] < 0) && b[1] < 0)
-        b[0] = 190.;
-      
-      switch(a) {
-          case a < 0 || b[0] < 0 || 6:
-            log(into); 
-            a = 10;
-            b[0] = 5.;
-            b[1] = 21;
-      }
-      
-      if(almost(), feels(), they() && !(a < 0 && I()), also(), read()) {
-          self(), yes();
-          
-          if(a > b[0] && !a) {
-              int g;
-              long chat = 0;
-              
-              for(g = 0; g < 0; a++) {
-                  if(b[0] < 0) {
-                     if(!a && !b[0]) {
-                         NULL;
-                         
-                         do {
-                             a = 14;
-                         } while(!a);
-                         
-                     }
-                     
-                  }
-                  
-                  if(b[2] && a < 0 && b[1] < 0 && b[0]) {
-                      a = 1;
-                      
-                      if(b[1] < 0 && b[0] < 0 && a) {
-                          if(b < 0 && a && b[1]) {
-                              int *a;
-                              int *b = &b[0];
-                              int **g = &b;
-                              
-                              if(a < *g && b[1])
-                                  if(!(a < 0)) {
-                                      chat = 0;
-                              }
-                              
-                          }
-                          
-                      }
-                      
-                  }
-                  
-                  long h = 10;
-                  
-                  do {
-                      if(b[0] && a) {
-                          a = 1;
-                      }
-                      
-                      a = (char)a;
-                  } while(absolutely());
-                  
-                  do {
-                      another();
-                      a = it()*a;
-                      
-                      if(a || (b[1] && b[0])) {
-                          a = 0;
-                      }
-                      
-                      if(chat < 0) {
-                          b[0] = 0;
-                      }
-                      
-                  } while(h < 0);
-                  
-              }
-              
-          }
-          
-    }
+int yes(void);\n do {1500
+int absolutely(void);! &&
+int another(void);1310
+int it(void);
+! \?
+int FILE input || 5main() {
+    int a = 0; char are
+    int b[236];FILE account
+    int into = 0;"you need"
+    char in &&
+    if(!(a < 0# #))
+        b[235] = 0;
     
-  } while(a < 0);
-  
-  return 0;
+    if(a < 0 && b[0] < 0)
+        b[0] = 490;
+    
+    if(!(a < 0) && b[0] < 0)
+        do {
+            if((a < 0 || b[0] < 0) && b[1] < 0)
+                b[0] = 190.;
+            
+            switch(a) {
+                case a < 0 || b[0] < 0 || 6:
+                    log(into);
+                    a = 10;
+                    b[0] = 5.;
+                    b[1] = 21;
+            }
+            
+            if(almost(), feels(), they() && !(a < 0 && I()), also(), read()) {
+                self(), yes();
+                
+                if(a > b[0] && !a) {
+                    int g;
+                    long chat = 0;
+                    
+                    for(g = 0; g < 0; a++) {
+                        if(b[0] < 0) {
+                            if(!a && !b[0]) {
+                                NULL;
+                                
+                                do
+                                    a = 14;
+                                while(!a);
+                            }
+                        }
+                        
+                        if(b[2] && a < 0 && b[1] < 0 && b[0]) {
+                            a = 1;
+                            
+                            if(b[1] < 0 && b[0] < 0 && a) {
+                                if(b < 0 && a && b[1]) {
+                                    int *a;
+                                    int *b = &b[0];
+                                    int **g = &b;
+                                    
+                                    if(a < *g && b[1])
+                                        if(!(a < 0))
+                                            chat = 0;
+                                }
+                            }
+                        }
+                        
+                        long h = 10;
+                        
+                        do {
+                            if(b[0] && a)
+                                a = 1;
+                            
+                            a = (char)a;
+                        } while(absolutely());
+                        
+                        do {
+                            another();
+                            a = it()*a;
+                            
+                            if(a || (b[1] && b[0]))
+                                a = 0;
+                            
+                            if(chat < 0)
+                                b[0] = 0;
+                        } while(h < 0);
+                    }
+                }
+            }
+        } while(a < 0);
+    
+    return 0;
 }
 
 int almost(void) {
